053656506a890c44b4d9026beb9f76b88b8d2206
João Bispo <vitor.vi10@hotmail.com>
2020-08-02 13:22:25 -0300

    chore: other form of feat new task
    "which way is better?": insertAdjacentHTML vs innerHTML.

--- manipulate-dom/listaDeTarefas.js
+++ manipulate-dom/listaDeTarefas.js
@@ -1,18 +1,39 @@
 $ = document.querySelector.bind(document);
 
 const button = $('[data-form-button]');
-const list = $('.list');
+const list = $('[data-list]');
 
-button.addEventListener('click', newTask);
+button.addEventListener('click', criarTarefa);
 
 function newTask(event) {
     event.preventDefault();
 
-    let input = $('[data-form-input]');
+    const input = $('[data-form-input]');
 
-    const task = `<p class="task">${input.value}</p>`;
+    const value = input.value;
+
+    const task = `
+    <li class="task"><p class="content">${value}</p></li>
+    `;
 
     list.insertAdjacentHTML('beforeend', task);
+
+    input.value = '';
+}
+
+function criarTarefa(event) {
+    event.preventDefault();
+
+    const input = $('[data-form-input]');
+    const value = input.value;
+
+    const task = document.createElement('li');
+    task.classList.add('task');
+    const content = `<p class="content">${value}</p>`    
+
+    task.innerHTML = content;
+
+    list.appendChild(task);
 
     input.value = '';
 }
