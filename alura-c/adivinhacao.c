4391382dadd2534d6d5c23b4706c00c3551e35d1
João Bispo <vitor.vi10@hotmail.com>
2020-10-28 15:16:58 -0300

    :paperclip: chore: if else and loopings in c

--- alura-c/adivinhacao.c
+++ alura-c/adivinhacao.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#define INFINITY 1
 
 int main() {
 
-    printf("================================\n");
-    printf("Bem-vindo ao jogo de advinhacao!\n");
-    printf("================================\n");
+    printf("\n\n\n");
+    printf("+================================+\n");
+    printf("|Bem-vindo ao jogo de advinhacao!|\n");
+    printf("+================================+\n");
 
-    int secretNumber = 42;   
-
+    int secretNumber = 42;
     int luckyNumber;
+    int attempts = 1;
 
     printf("Preparado para tentar a sua sorte?!\n");
 
-    for(int i = 1; i <= 3; i =  i + 1) {
+    while(INFINITY){
 
-      printf("\nTentativa %d de 3\n\n", i);
+        printf("\nTentativa: %d.\n\n", attempts);
 
-      printf("Digite um número para testar a sorte: ");
-      scanf("%i", &luckyNumber);
+        printf("Digite um número para testar a sorte: ");
+        scanf("%i", &luckyNumber);
 
-      printf("\nMacacos me mordam! \nO número que você digitou foi: %i\n", luckyNumber);
+        if (luckyNumber < 0){
+            printf("+============================================================+\n");
+            printf("|Tá de brincadeira? VOCÊ PRECISA DIGITAR UM NÚMERO POSITIVO! |\n");
+            printf("+============================================================+\n");
+            printf("\n\n\n\n");
+            continue;
+        }
+        
+        printf("\nMacacos me mordam! \nO número que você digitou foi: %i\n\n\n", luckyNumber);
 
-      int isEqual = (luckyNumber == 42);
-      
+        int isEqual = (luckyNumber == 42);
+        int isBiggerThan = (luckyNumber > 42);
 
-      if (isEqual) {
-          printf("Wow, você por acaso é o Albert Einstein? Acertou!\n");
-          printf(".\n.\nContinue jogando para treinar sua sorte!\n");
+        if (isEqual) {
+            printf("Wow, você por acaso é o Albert Einstein? Acertou!\n");
+            printf(".\n.\nContinue jogando para treinar sua sorte!\n");
 
-          break;
-      } else {
-          int isBiggerThan = (luckyNumber > 42);
-          
-          if (isBiggerThan) {
-              printf(".\nDessa vez foi QUASE, mas ainda sim, o número que você digitou é maior que o número da sorte\n");
-          } else {
-              printf(".\nDessa vez foi QUASE, mas ainda sim, o número que você digitou é menor que o número da sorte\n");
-          }            
-      }
+            break;
+        } else if (isBiggerThan) {       
+            printf(".\nDessa vez foi QUASE, mas ainda sim, o número que você digitou é maior que o número da sorte\n");
+        } else {
+            printf(".\nDessa vez foi QUASE, mas ainda sim, o número que você digitou é menor que o número da sorte\n");
+        }            
+        attempts++;
     }
 
     printf("\nFim de jogo!\n");
+    printf("\nO seu número de tentativas foi: %d!\n", attempts);
     
 }
