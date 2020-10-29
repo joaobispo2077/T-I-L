4edb42deb162e96acdaae91ff54942f5ec55f962
João Bispo <vitor.vi10@hotmail.com>
2020-10-29 17:44:22 -0300

    :paperclip: chore:  add do while to the game

--- alura-c/jogo-forca.c
+++ alura-c/jogo-forca.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-   char secretWord[20];
+	char secretWord[20];
 
-   int hasWon = 0;
-   int hasLose = 0;
+	sprintf(secretWord, "BATATA");
 
-   do
-   {
-     
-   } while (!hasWon && !hasLose);
-   
+	int hasWon = 0;
+	int hasLose = 1;
+
+	do{
+
+		char luckyWord;
+		printf("Digite uma letra: ");
+		scanf("%c", &luckyWord);
+
+		for (int i = 0; i < strlen(secretWord); i++){
+			if (secretWord[i] == luckyWord){
+				printf("Acertou! Essa letra está na posição %i\n", i);
+			}
+			
+		}
+		
+
+	} while (!hasWon && !hasLose);
+	
 }
