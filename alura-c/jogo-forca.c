5d4c18476ad96c5c75b836c30886a6dc6198b9ad
João Bispo <vitor.vi10@hotmail.com>
2020-10-30 23:34:13 -0300

    :paperclip: chore: add pointers and functions to the game

--- alura-c/jogo-forca.c
+++ alura-c/jogo-forca.c
@@ -7,18 +7,69 @@ void showWelcome() {
 	printf("+------------------------------+\n\n");
 }
 
-void tryGuessing(char luckyWords[26], int attempts) {
+void tryGuessing(char luckyWords[26], int* attempts) {
 	char luckyWord;
 	printf("\n\nDigite uma letra: ");
 	scanf(" %c", &luckyWord);
 
-	luckyWords[attempts] = luckyWord;
+	luckyWords[(*attempts)] = luckyWord;
+	(*attempts)++;
+}
+
+int verifyGuesses(char words, char luckyWords[26], int attempts) {
+	int hasFind = 0;
+
+	for (int j = 0; j < attempts; j++) {
+		if (luckyWords[j] == words) {
+			hasFind = 1;
+			break;
+		}
+	}
+
+	return hasFind;
+}
+
+void choiceWord(char secretWord[20]) {
+	sprintf(secretWord, "BATATA");
+}
+
+void drawnGallows(char secretWord[20], char luckyWords[26], int attempts) {
+		for (int i = 0; i < strlen(secretWord); i++){
+
+			int hasFind = verifyGuesses(secretWord[i],	luckyWords, attempts);
+
+			if(hasFind) {
+				printf(" %c ", secretWord[i]);
+			} else {
+				printf(" _ ");
+			}
+		
+	}	
+}
+
+int handleLose(int attempts,  char secretWord[20], char luckyWords[26]) {
+	int errors = 0;
+
+	for (int i = 0; i < attempts; i++) {
+		int isExist = 0;
+
+		for (int j = 0; j < attempts; j++) {
+			if (luckyWords[i] == secretWord[j]) {
+				isExist = 1;
+			}			
+		}
+
+		if (!isExist) errors++;
+		
+	}
+
+	return errors >= 5;
 }
 
 int main() {
 	char secretWord[20];
 
-	sprintf(secretWord, "BATATA");
+	choiceWord(secretWord);
 
 	int hasWon = 0;
 	int hasLose = 0;
@@ -29,28 +80,11 @@ int main() {
 	showWelcome();
 
 	do{
-		
-	for (int i = 0; i < strlen(secretWord); i++){
-		int hasFind = 0;
+		drawnGallows(secretWord, luckyWords, attempts);
 
-		for (int j = 0; j < attempts; j++) {
-			if (luckyWords[j] == secretWord[i]) {
-				hasFind = 1;
-				break;
-			}
-		}
+		tryGuessing(luckyWords, &attempts);
 
-			if(hasFind) {
-				printf(" %c ", secretWord[i]);
-			} else {
-				printf(" _ ");
-			}
-		
-	}	
-	
-	tryGuessing(luckyWords, attempts);
-	attempts++;
-
+		hasLose = handleLose(attempts, secretWord, luckyWords);
 	} while (!hasWon && !hasLose);
 	
 }
