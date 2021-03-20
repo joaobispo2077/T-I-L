a44f36d1c3abea654f0c06834e829f207ff906ce
João Bispo <vitor.vi10@hotmail.com>
2021-03-20 17:48:12 -0300

    initialize strings revision in c

---
+++ data-structures/EDDA2/learning/strings/init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <string.h>
+int main()
+{
+  char word[] = {'T', 'o', 'r', 't', 'a'};
+
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%c", word[i]);
+  }
+  return 0;
+}
