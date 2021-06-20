8d4c9094e0600710a131146176b42820344757a2
João Bispo <joaobispo2077@gmail.com>
2021-06-20 13:45:21 -0300

    feat: add useContext in a single file

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,148 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {
-  useCallback,
-  useState,
-  useEffect,
-  useMemo,
-  useRef,
-} from 'react';
-// import { Component } from 'react';
-import Proptypes from 'prop-types';
+import React, { useContext, useState } from 'react';
 
-const Post = ({ post, onClick }) => {
-  console.log('Render Post');
+const globalInitialState = {
+  header: 'titulo legal do header',
+  title: 'Titulo do contexto',
+  footer: 'rodape da pagina',
+  counter: 0,
+};
 
+const GlobalContext = React.createContext();
+
+const Header = () => {
+  const context = useContext(GlobalContext);
+  const {
+    contextState: { header, counter },
+    contextState,
+    setContextState,
+  } = context;
   return (
-    <div>
-      <h1 onClick={(event) => onClick(event.target.innerHTML)}>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
+    <header
+      onClick={() => setContextState({ ...contextState, counter: counter + 1 })}
+    >
+      {header} - {counter}
+    </header>
   );
 };
 
-Post.propTypes = {
-  post: Proptypes.shape({
-    id: Proptypes.number,
-    title: Proptypes.string,
-    body: Proptypes.string,
-  }),
-  onClick: Proptypes.func,
+// eslint-disable-next-line
+const H1 = () => {
+  const context = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = context;
+  return (
+    <h1>
+      {title} - {counter}
+    </h1>
+  );
 };
 
-const Button = ({ incrementButton }) => {
-  console.log('Render button');
-
-  return <button onClick={() => incrementButton(10)}>+</button>;
+// eslint-disable-next-line
+const Box = () => {
+  return (
+    <>
+      <Header />
+      <H1 text="Hello world" />
+      <Footer />
+    </>
+  );
 };
 
-Button.displayName = 'Button';
+const Footer = () => {
+  const context = useContext(GlobalContext);
+  const { contextState, setContextState } = context;
 
-Button.propTypes = {
-  incrementButton: Proptypes.func,
+  const handleChangeFooter = () => {
+    console.log(contextState);
+    const newContext = { ...contextState, footer: contextState.footer + '!' };
+
+    setContextState(newContext);
+  };
+
+  return <footer onClick={handleChangeFooter}>{contextState.footer}</footer>;
 };
-
-// const handleClicked = () => {
-//   console.log('h1 clicado');
-// };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [input, setInput] = useState('');
-
-  const inputRef = useRef(null);
-
-  const handleChangeRotation = () => {
-    setReverse(!reverse);
-  };
-
-  const handleIncrementCounter = useCallback((number) => {
-    setCounter((counter) => counter + number);
-  }, []);
-
-  useEffect(() => {
-    console.log(`counter atualizou para: ${counter} [componentDidUpdate]`);
-  }, [counter]);
-
-  // useEffect(() => console.log('componentDidUpdate'));
-
-  // useEffect(() => {
-  //   console.log('componentDidMount');
-  //   /*trash in component*/
-  //   document.querySelector('h1')?.addEventListener('click', handleClicked);
-
-  //   //component will unmount
-  //   return () => {
-  //     document.querySelector('h1')?.removeEventListener('click', handleClicked);
-  //   };
-  // }, []);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((posts) => setPosts(posts));
-  }, []);
-
-  const memoButton = useMemo(
-    () => <Button incrementButton={handleIncrementCounter} />,
-    [handleIncrementCounter],
-  );
-
-  const handlePostToInput = (text) => {
-    console.log('post to input', text);
-    setInput(text);
-  };
-
-  useEffect(() => {
-    console.log(inputRef.current);
-    inputRef.current.focus();
-  }, [input]);
+  const [contextState, setContextState] = useState(globalInitialState);
 
   console.log('Render app');
   return (
-    <div className="App">
-      <header className="App-header">
-        <input
-          ref={inputRef}
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <img
-          src={logo}
-          className={`App-logo ${reverse ? 'reverse' : ''}`}
-          alt="logo"
-        />
-
-        {useMemo(
-          () =>
-            posts &&
-            posts.map((post) => (
-              <Post onClick={handlePostToInput} key={post.id} post={post} />
-            )),
-          [posts],
-        )}
-
-        {posts.length <= 0 && <p>Ainda não existem posts</p>}
-        <h1>Contador: {counter}</h1>
-        {memoButton}
-        <p onClick={handleChangeRotation}>
-          {reverse
-            ? 'Rodando no sentido Horário'
-            : 'Rodando no sentido anti-horário'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Box />
+    </GlobalContext.Provider>
   );
 }
 
