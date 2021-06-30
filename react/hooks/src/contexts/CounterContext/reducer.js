6feefb5c5ecaded62a80866b73b7b904fd04630b
João Bispo <joaobispo2077@gmail.com>
2021-06-30 20:42:12 -0300

    feat: add actions of Counter Context

---
+++ react/hooks/src/contexts/CounterContext/reducer.js
@@ -0,0 +1,36 @@
+import { initialState } from '.';
+import * as actionTypes from './action-types';
+
+// receive state and trade state
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.INCREASE: {
+      return { ...state, counter: state.counter + 1 };
+    }
+
+    case actionTypes.DECREASE: {
+      return { ...state, counter: state.counter - 1 };
+    }
+
+    case actionTypes.RESET: {
+      return { ...initialState };
+    }
+
+    case actionTypes.SET_COUNTER: {
+      return { ...state, ...action.payload };
+    }
+
+    case actionTypes.ASYNC_INCREASE_START: {
+      return { ...state, loading: true };
+    }
+
+    case actionTypes.ASYNC_INCREASE_END: {
+      return { ...state, loading: false };
+    }
+
+    case actionTypes.ASYNC_INCREASE_ERROR: {
+      return { ...state, loading: false };
+    }
+  }
+  return state;
+};
