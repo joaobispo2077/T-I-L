ad4bb6b214b32314043c89400c1465a30e78863d
João Bispo <joaobispo2077@gmail.com>
2021-07-11 17:55:09 -0300

    feat: add tables

--- college/BD2A3/project/tables.sql
+++ college/BD2A3/project/tables.sql
@@ -7,35 +7,96 @@ CREATE TABLE IF NOT EXISTS usuario (
   lastname VARCHAR(25),
   idade TINYINT UNSIGNED,
   apelido VARCHAR(255),
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
   senha VARCHAR(255) NOT NULL
 );
 
--- TODO: Validar formato da data e hora do usuário
-CREATE TABLE IF NOT EXISTS tarefa(
+CREATE TABLE IF NOT EXISTS tarefa (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   titulo VARCHAR(50) NOT NULL,
   descricao VARCHAR(144) NOT NULL,
   status ENUM("PENDENTE", "EM ANDAMENTO", "RESOLVIDO") DEFAULT 'PENDENTE' NOT NULL,
   tag VARCHAR(20),
-  data DATE NOT NULL DEFAULT NOW(),
-  hora
+  data DATE NOT NULL,
+  hora TIME NOT NULL,
   usuario_id INT UNSIGNED NOT NULL,
   CONSTRAINT fk_usuario_id
 		FOREIGN KEY (usuario_id)
         REFERENCES usuario(id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE,
+        ON DELETE CASCADE
 );
 
-	data_consulta date not null,
-    hora time not null,
-    codigo_medico int unsigned,
-    codigo_paciente int unsigned,
-    primary key (data_consulta, hora, codigo_medico),
-    constraint fk_codigo_medico
-		foreign key (codigo_medico)
-        references medicos (codigo_medico)
-        on update cascade
-        on delete cascade
-)
+
+CREATE TABLE IF NOT EXISTS subtarefa (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  descricao VARCHAR(144) NOT NULL,
+  usuario_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (usuario_id)
+    REFERENCES usuario(id)
+     ON UPDATE CASCADE
+     ON DELETE CASCADE
+);
+
+CREATE TABLE subtarefa_tarefa (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNSIGNED NOT NULL,
+  tarefa_id INT UNSIGNED NOT NULL,
+  subtarefa_id INT UNSIGNED NOT NULL,
+  concluido BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (tarefa_id) 
+    REFERENCES tarefa(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+  FOREIGN KEY (usuario_id)
+    REFERENCES usuario(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  FOREIGN KEY (subtarefa_id)
+    REFERENCES subtarefa(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE lembrete(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  data DATE NOT NULL,
+  hora TIME NOT NULL,
+  usuario_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (usuario_id)
+    REFERENCES usuario(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE lembrete_tarefa (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNSIGNED NOT NULL,
+  tarefa_id INT UNSIGNED NOT NULL,
+  lembrete_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (tarefa_id) 
+    REFERENCES tarefa(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+  FOREIGN KEY (usuario_id)
+    REFERENCES usuario(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  FOREIGN KEY (lembrete_id)
+    REFERENCES lembrete(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE IF NOT EXISTS usuario (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(15) NOT NULL,
+  lastname VARCHAR(25),
+  idade TINYINT UNSIGNED,
+  apelido VARCHAR(255),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  senha VARCHAR(255) NOT NULL
+);
+
