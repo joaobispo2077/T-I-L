6e2c5c6b6cecd50bd6a92a7cc5d578d18673e12c
João Bispo <joaobispo2077@gmail.com>
2021-07-27 20:37:16 -0300

    feat: setup nextjs project and add styled-components

---
+++ nextjs/renderizer/src/pages/_app.jsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { theme } from '../styles/theme'
+
+
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ nextjs/renderizer/src/styles/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#067963',
+  },
+}
