de965b4cc69f52ee6bffed4c5cd42232b57f37ee
João Bispo <joaobispo2077@gmail.com>
2021-07-27 08:25:12 -0300

    chore: task15 BD2A3

---
+++ college/BD2A3/task15/index.sql
@@ -0,0 +1,40 @@
+CREATE PROCEDURE remover_lembretes_da_tarefa_do_usuario(IN id_usuario INT, IN id_tarefa INT)
+DELETE FROM lembrete_tarefa WHERE usuario_id = id_usuario AND tarefa_id = id_tarefa;
+
+CALL remover_lembretes_da_tarefa_do_usuario(1, 1);
+
+
+CREATE PROCEDURE remover_tarefa_do_usuario(IN id_usuario INT, IN id_tarefa INT)
+DELETE FROM tarefa WHERE usuario_id = id_usuario AND id = id_tarefa;
+
+CALL remover_tarefa_do_usuario(2, 1);
+
+CREATE PROCEDURE remover_todas_tarefa_do_usuario(IN id_usuario INT)
+DELETE FROM tarefa WHERE usuario_id = id_usuario;
+
+CALL remover_todas_tarefa_do_usuario(3);
+
+DELIMITER $
+ CREATE FUNCTION bem_vindo() RETURNS CHAR(100) DETERMINISTIC
+ BEGIN
+  RETURN "Bem-vindo ao seu agendador de tarefas!";
+ END
+ $
+
+DELIMITER $$
+CREATE FUNCTION lista_todos_usuarios()
+BEGIN
+  SELECT * FROM usuario;
+  RETURN
+END
+$$
+
+DELIMITER ||
+CREATE FUNCTION listar_tarefas_do_usuario(IN id_usuario INT)
+BEGIN
+  SELECT id FROM tarefa WHERE usuario_id = id_usuario;
+  RETURN
+END
+||
+
+
