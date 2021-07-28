153744e90e4add4bbdb7f5b70bd2b0282fffd8ac
João Bispo <joaobispo2077@gmail.com>
2021-07-28 15:17:01 -0300

    feat: setup jest with next js

---
+++ nextjs/renderizer/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/templates/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+  ],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+};
