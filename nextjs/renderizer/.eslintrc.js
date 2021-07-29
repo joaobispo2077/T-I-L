bc8d3edf47deeeacb91401bbb5b8b05911ade890
João Bispo <joaobispo2077@gmail.com>
2021-07-29 16:08:22 -0300

    feat: config eslint with next and typescript

--- nextjs/renderizer/.eslintrc.js
+++ nextjs/renderizer/.eslintrc.js
@@ -9,10 +9,12 @@ module.exports = {
     'next',
     'next/core-web-vitals',
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -20,7 +22,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   settings: {
     react: {
       version: 'detect',
@@ -28,6 +30,7 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'import/no-anonymous-default-export': 'off',
     '@next/next/no-img-element': 'off',
   },
