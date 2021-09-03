75ea128a8e1eeec47c64a761607f64f05e7e5ee1
João Bispo <joaobispo2077@gmail.com>
2021-09-03 10:23:39 -0300

    feat: add component side bar

--- rocket/ignite/reactjs/ignite-template-componentizando-a-aplicacao/src/App.tsx
+++ rocket/ignite/reactjs/ignite-template-componentizando-a-aplicacao/src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
+import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
 import { api } from './services/api';
@@ -13,7 +13,7 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
-interface GenreResponseProps {
+export interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
@@ -60,22 +60,11 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar
+        genres={genres}
+        selectedGenreId={selectedGenreId}
+        onRequestClickGenre={handleClickButton}
+      />
 
       <Content category={selectedGenre.title} movies={movies}/>
     </div>
--- rocket/ignite/reactjs/ignite-template-componentizando-a-aplicacao/src/components/SideBar.tsx
+++ rocket/ignite/reactjs/ignite-template-componentizando-a-aplicacao/src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+export type SideBarProps = {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onRequestClickGenre: (id: number) => void;
+};
+
+export function SideBar({ genres, selectedGenreId ,onRequestClickGenre } : SideBarProps) {
+ return (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onRequestClickGenre(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+ );
 }
