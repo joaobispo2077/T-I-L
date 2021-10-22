8def38faf90f703a563472ac39451797820f69f8
João Bispo <joaobispo2077@gmail.com>
2021-10-21 21:06:53 -0300

    feat: setup modules valid mask

---
+++ college/DW2A4/modules-valid-mask/src/js/main.js
@@ -0,0 +1,15 @@
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+// const inputNodeList = $$('input');
+// const inputList = Array.from(inputNodeList);
+
+// console.log(inputList);
+
+// inputList.forEach(input => {
+//   input.addEventListener('input', () => {
+//     $event.target.value = ;
+//     console.log(input.id, input.value);
+//   });
+
+// });
