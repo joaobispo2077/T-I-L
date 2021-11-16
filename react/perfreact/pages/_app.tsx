c81399c3e4a53bf88fee45c6049fd1d7cff110e1
João Bispo <joaobispo2077@gmail.com>
2021-11-16 18:22:48 -0300

    feat: resetup nextjs app perfreact with typescript

--- react/perfreact/pages/_app.tsx
+++ react/perfreact/pages/_app.tsx
@@ -1,4 +1,7 @@
-function MyApp({ Component, pageProps }) {
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
