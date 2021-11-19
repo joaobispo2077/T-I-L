6cfcc37538903dfa897d044cc48c15f32707c8df
João Bispo <joaobispo2077@gmail.com>
2021-11-19 11:45:45 -0300

    feat: add useMemo

--- react/perfreact/src/components/SearchResult.tsx
+++ react/perfreact/src/components/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 type Product = {
@@ -12,11 +12,23 @@ type SearchResultProps = {
 };
 
 export function SearchResult({ results }: SearchResultProps) {
+  const quantity = useMemo(() => results.length, [results]);
+
   return (
     <div>
+      <h2>Quantidade: {quantity}</h2>
       {results.map((product) => (
         <ProductItem key={product.id} product={product}/>
       ))}
     </div>
   );
 }
+
+/**
+ * useMemo - React hook para memorizar um valor
+ *
+ * Use case:
+ *
+ * 1. Cálculos pesados
+ * 2. Igualdade referêncial (Quando um valor é repassado para um componente filho)
+ */
