fd0fdd619098d2c9a8de08d2830528b93716dbc8
João Bispo <joaobispo2077@gmail.com>
2021-11-19 11:24:04 -0300

    feat: add react memo

--- react/perfreact/src/components/ProductItem.tsx
+++ react/perfreact/src/components/ProductItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {memo} from "react";
 
 type Product = {
   id: number;
@@ -10,7 +10,7 @@ type ProductItemProps = {
   product: Product;
 };
 
-const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+const ProductItemComp: React.FC<ProductItemProps> = ({ product }) => {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
@@ -18,4 +18,22 @@ const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
   );
 };
 
+const hasPropsChanged = (prevProps: ProductItemProps, nextProps: ProductItemProps) => {
+  return Object.is(prevProps.product, nextProps.product);
+};
+
+// memo - before create a component new version, check if props has changed
+const ProductItem = memo(ProductItemComp, hasPropsChanged); // default second argument is a shallow comparison
+
 export { ProductItem };
+
+/**
+ * Memo is a performance optimization that only renders a component if its props or state have changed.
+ *
+ * Use Case:
+ *
+ * 1. Pure Functional Component - Same html, same props, same state according to the props
+ * 2. Renders to often with same props (critical)- Renders a lot of times according to the many intereactions
+ * 3. Re-renders with the same props
+ * 4. Medium to big component
+*/
