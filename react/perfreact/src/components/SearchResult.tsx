d1ad807243fdaa9e7e19288e685a43a49e1a0e56
João Bispo <joaobispo2077@gmail.com>
2021-11-23 11:50:54 -0300

    feat: add virtualization to improve performance

--- react/perfreact/src/components/SearchResult.tsx
+++ react/perfreact/src/components/SearchResult.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
+import { AutoSizer, List, ListRowRenderer } from "react-virtualized";
 
 type Product = {
   id: number;
@@ -16,12 +17,29 @@ type SearchResultProps = {
 export function SearchResult({ results, onAddToWishlist }: SearchResultProps) {
   const quantity = useMemo(() => results.length, [results]);
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>Quantidade: {quantity}</h2>
-      {results.map((product) => (
-        <ProductItem key={product.id} product={product} onAddToWishlist={onAddToWishlist}/>
-      ))}
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
