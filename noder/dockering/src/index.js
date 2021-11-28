c8f9f47bc89b66f45778fab99290ba473effd666
João Bispo <joaobispo2077@gmail.com>
2021-11-28 20:55:01 -0300

    feat: ignode node_modules into .dockerignore

--- noder/dockering/src/index.js
+++ noder/dockering/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (_, res) => res.send('Hello World!'));
+app.get('/', (_, res) => res.json({ message: 'Hello World!' }));
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
