b936da87b25f734b788614807296e2dd7ca3fc46
João Bispo <joaobispo2077@gmail.com>
2021-12-12 18:24:04 -0300

    feat: setup react SOLID components app

---
+++ rocket/experts/dashboard-dinamico-reutilizavel-solid-react-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Dashboard} from 'src/components/Dashboard/Dashboard';
+
+import './styles/main.scss';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Dashboard />
+		</div>
+	);
+};
+
+export default App;
