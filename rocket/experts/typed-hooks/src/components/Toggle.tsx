7573a7f3a3dd8dba66b559be41de168de31e56fe
João Bispo <joaobispo2077@gmail.com>
2021-12-14 17:50:17 -0300

    feat: add custom useLocalStorage

--- rocket/experts/typed-hooks/src/components/Toggle.tsx
+++ rocket/experts/typed-hooks/src/components/Toggle.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useToggle } from "../hooks/useToggle";
 
 const Toggle = (props: any) => {
-  const [toggled, toggle] = useToggle(false);
+  const [savedToggle, setSavedToggle] = useLocalStorage('darkmode', false);
+  const [toggled, toggle] = useToggle(savedToggle);
+
+
+  const handleToggle = () => {
+    toggle();
+    setSavedToggle(toggled);
+    console.log(toggled);
+  };
 
   return (
     <div className="dark-mode-toggle">
-      <button type="button" onClick={toggle}>
+      <button type="button" onClick={handleToggle}>
         ☀
       </button>
       <span className="toggle-control">
@@ -14,12 +23,12 @@ const Toggle = (props: any) => {
           className="dmcheck"
           id="dmcheck"
           type="checkbox"
-          checked={toggled}
-          onChange={toggle}
+          checked={!toggled}
+          onChange={handleToggle}
         />
         <label htmlFor="dmcheck" />
       </span>
-      <button type="button" onClick={toggle}>
+      <button type="button" onClick={handleToggle}>
         ☾
       </button>
     </div>
