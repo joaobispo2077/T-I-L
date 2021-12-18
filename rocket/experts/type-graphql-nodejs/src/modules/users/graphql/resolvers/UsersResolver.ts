d13e2f48042a034ba3008a5616259e1b788b1388
João Bispo <joaobispo2077@gmail.com>
2021-12-18 17:45:02 -0300

    feat: add Users Resolver

---
+++ rocket/experts/type-graphql-nodejs/src/modules/users/graphql/resolvers/UsersResolver.ts
@@ -0,0 +1,28 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../../database/entities/User";
+
+@Resolver()
+export class UsersResolver {
+  @Query(() => [User])
+  async getUsers(): Promise<User[]> {
+    return User.find();
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("firstname") firstname: string,
+    @Arg("lastname") lastname: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const user = Object.assign(new User(), {
+      firstname,
+      lastname,
+      email,
+      password,
+    });
+
+    await User.save(user);
+    return user;
+  }
+}
