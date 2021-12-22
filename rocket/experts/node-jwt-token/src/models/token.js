145e020e78cb0d6be1e1cda6249b1985034d101f
João Bispo <joaobispo2077@gmail.com>
2021-12-22 09:59:48 -0300

    feat: setup jwt seeds, model and migrations

---
+++ rocket/experts/node-jwt-token/src/models/token.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const schema = {
+    user_id: DataTypes.STRING,
+    token: DataTypes.STRING,
+    valid: DataTypes.BOOLEAN,
+    expires: DataTypes.DATE,
+  }
+
+  const modelOptions = {
+    tableName: 'tokens',
+  }
+
+  const Token = sequelize.define('Token', schema, modelOptions)
+
+  return Token
+}
