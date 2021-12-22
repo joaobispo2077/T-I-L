35e2faabf1c3b4fd00a53667f271ce91ea64c076
João Bispo <joaobispo2077@gmail.com>
2021-12-22 15:42:29 -0300

    feat: add script to parse ugly OF

---
+++ custom_scripts/resolvers/resolve_of.js
@@ -0,0 +1,43 @@
+const { promisify } = require('util');
+const { resolve } = require('path');
+const { readFile, writeFile } = require('fs');
+
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+
+const parseFilelineToArtefactList = line => {
+  const [url, commitHash] = line.split('#');
+
+  const isAnArtefact = url && commitHash;
+  if (!isAnArtefact) {
+    return line;
+  }
+
+  const firstTencommitHashCharacters = commitHash ? commitHash.slice(0, 10) : '';
+  const artefact = [url, firstTencommitHashCharacters].join('#');
+
+  return artefact;
+}
+
+const resolveOF = async () => {
+  const rootDirectory = resolve(__dirname);
+  const sourceFile = resolve(rootDirectory, '..', 'data', 'of.txt');
+
+  const fileData = await readFileAsync(sourceFile, 'utf8');
+
+  const fileLines = fileData.split('\n');
+  console.info(`\n=== processing ${fileLines.length} lines ===\n`);
+
+  const artefactList = fileLines.map(parseFilelineToArtefactList);
+
+  const newFileData = artefactList.join('\n');
+
+  const targetFile = resolve(rootDirectory, '..', 'data', 'of_resolved.txt');
+  await writeFileAsync(targetFile, newFileData);
+
+  console.info("\n===output===\n");
+  console.info(newFileData);
+
+}
+
+resolveOF();
