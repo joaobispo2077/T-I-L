7936507357458c9277e890a63837921c4cd99528
João Bispo <joaobispo2077@gmail.com>
2022-02-22 09:19:32 -0300

    feat: add CreateChallengeSubmissionUseCase

--- rocket/youtube/decode/010-node-api/src/application/useCases/CreateChallengeSubmissionUseCase.ts
+++ rocket/youtube/decode/010-node-api/src/application/useCases/CreateChallengeSubmissionUseCase.ts
@@ -1,4 +1,6 @@
 import { Submission } from '../../domain/entities/submission';
+import { ChallengesRepository } from '../repositories/ChallengesRepository';
+import { StudentsRepository } from '../repositories/StudentsRepository';
 
 type CreateChallengeSubmissionRequest = {
   studentId: string;
@@ -6,7 +8,26 @@ type CreateChallengeSubmissionRequest = {
 };
 
 export class CreateChallengeSubmission {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private challengesRepository: ChallengesRepository,
+  ) {}
+
   async execute({ challengeId, studentId }: CreateChallengeSubmissionRequest) {
+    const isStudentExist = await this.studentsRepository.findById(studentId);
+
+    if (!isStudentExist) {
+      throw new Error('Student not found');
+    }
+
+    const isChallengeExist = await this.challengesRepository.findById(
+      challengeId,
+    );
+
+    if (!isChallengeExist) {
+      throw new Error('Challenge not found');
+    }
+
     const submission = Submission.create({
       challengeId,
       studentId,
--- rocket/youtube/decode/010-node-api/src/core/domain/Entity.ts
+++ rocket/youtube/decode/010-node-api/src/core/domain/Entity.ts
@@ -4,6 +4,10 @@ export abstract class Entity<GenericProps> {
   protected _id: string;
   public props: GenericProps;
 
+  get id(): string {
+    return this._id;
+  }
+
   constructor(props: GenericProps, id?: string) {
     this.props = props;
     this._id = id ?? crypto.randomUUID();
